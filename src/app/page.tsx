0607974d1cdd2149b93beded9025405edfadf2b7
Marko Bozic <markobozic346@gmail.com>
2023-10-16 14:43:34 +0200

    Add nest 13 cache with revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import axios from "axios";
 import HomePage from "./_home/HomePage";
 
 const fetchData = async () => {
-  return axios.get(
-    "https://script.google.com/macros/s/AKfycbx-IHfmCLu265z22d38-A-F-ZcQEBSmBesbOa1Z-d8dnZfzoPW36tIvQWygvjRF3hr7/exec"
+  const res = await fetch(
+    "https://script.google.com/macros/s/AKfycbx-IHfmCLu265z22d38-A-F-ZcQEBSmBesbOa1Z-d8dnZfzoPW36tIvQWygvjRF3hr7/exec",
+    { next: { revalidate: 300 } }
   );
+
+  return res.json();
 };
 
 export default async function Home() {
-  const { data } = await fetchData();
+  const data = await fetchData();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
