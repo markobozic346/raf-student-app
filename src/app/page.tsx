990df823d4677debd9a61ec2e2cd9290bb0f279a
Marko Bozic <markobozic346@gmail.com>
2024-01-13 21:21:34 +0100

    Add revalidate and dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HomePage from "./_home/HomePage";
 
+export const dynamic = "force-dynamic";
+
 const fetchData = async () => {
   const res = await fetch(
     "https://script.google.com/macros/s/AKfycbzsz2uYJKlGp04jISeQz9t5_fSFaAEBpdflm9jOxQ_iYSK9_3esG5eXxwilphbmrI1y/exec",
     {
       next: {
-        revalidate: 3600,
+        revalidate: 60,
       },
     }
   );
