094aa7399cb00a9049a22578a36d9f697f76a0c1
Marko Bozic <markobozic346@gmail.com>
2024-07-05 17:19:38 +0200

    Add schema for subject task

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,17 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { boolean, integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const table = pgTable("comment", {
-  int: integer("int"),
+export const subjectTask = pgTable("subject_task", {
+  id: integer("id").primaryKey(),
+  subject: varchar("subject", { length: 100 }).notNull(),
+  userId: integer("user_id").notNull(),
+  task: varchar("task", { length: 1000 }).notNull(),
+  deadline: timestamp("deadline", { withTimezone: true }).notNull(),
+  done: boolean("done").default(false).notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).default(
+    sql`CURRENT_TIMESTAMP`
+  ),
 });
